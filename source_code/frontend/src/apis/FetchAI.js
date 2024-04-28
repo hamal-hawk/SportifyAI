22b2e3238c3cbaf2b1263a7f6d612597d3e3bef1
hamal-hawk <Hamal@hawk.iit.edu>
2024-04-27 22:45:50 -0500

    External APIs integrated.

--- source_code/frontend/src/apis/FetchAI.js
+++ source_code/frontend/src/apis/FetchAI.js
@@ -4,7 +4,7 @@ export default function useFetchAI(prompt){
     const [dataAI, setDataAI] = useState(null);
     const [isPendingAI, setIsPendingAI] = useState(true);
     const [errorAI, setErrorAI] = useState(null);
-    const OPEN_AI_KEY = "YOUR_KEY_AI";
+    const OPEN_AI_KEY = "YOUR_KEY";
 
     useEffect(() => {
         if(prompt.length > 0){
